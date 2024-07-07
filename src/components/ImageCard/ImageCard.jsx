3f9af8ea0a1d5a8d34610214f4f3270a73bc692f
Spinkll <roma.skorohod.2005@gmail.com>
2024-07-07 11:19:06 +0300

    fix

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import css from "./ImageCard.module.css";
 
 const ImageCard = ({ image, onClick }) => (
-  <div className={css.card} onClick={() => onClick(image)}>
+  <div className={css.card}>
     <img
       src={image.urls.small}
       alt={image.alt_description}
       className={css.image}
+      onClick={() => onClick(image)}
     />
     <div className={css.info}>
       <p>{image.user.name}</p>
